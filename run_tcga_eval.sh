8ef22edee2d18f12fcf7cdaab7a333134d5630bb
alexanderm <alexanderm@aignostics.com>
2026-01-22 23:08:25 +0100

    Fix TCGA data root path in evaluation script

--- run_tcga_eval.sh
+++ run_tcga_eval.sh
@@ -4,4 +4,4 @@ python evaluate_benchmarks.py \
     --train_mpps 0.25 0.5 1.0 2.0 \
     --test_mpps 0.25 0.375 0.5 0.75 1.0 1.5 2.0 \
     --folds 0 1 2 3 4 \
-    --tcga_data_root /app/tcga_ms/tcga_ms
+    --tcga_data_root /app/tcga_ms
