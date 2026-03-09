491a9b56777366dc279e4ee8777033a33ad315ba
Florian C.F. Schulz <floriancf.schulz@gmail.com>
2026-03-09 21:37:37 +0100

    change path in scripts and add results folder

--- create_rankme_plot.sh
+++ create_rankme_plot.sh
@@ -3,7 +3,7 @@
 python magnification_analysis/plot_magnification_analysis_metrics.py \
     --model_name vits_du_s1 \
     --mpps 0.25 0.375 0.5 0.75 1.0 1.5 2.0 \
-    --tcga_data_root /root/TCGA-MS/tcga_ms \
-    --output_dir rankme_figures \
+    --tcga_data_root data/tcga_ms \
+    --output_dir results/rankme_figures \
     --skip_tsne \
     --batch_size 250 \
